da28ddcf8f7809c839fa4c727bc82b5f96a22f45
leon0113 <tahjib.leon399@gmail.com>
2022-10-29 01:17:47 +0600

    component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singers = [
+  { name: 'Arijit', song: 'tum ho' },
+  { name: 'Atif', song: 'wo lamhe' },
+  { name: 'Shreya', song: 'tum bin' },
+  { name: 'Eminem', song: 'venom' }
+
+];
 
 function App() {
+  const names = ['Rubel', 'Leon', 'Tahjib', 'Salman', 'Amir', 'Sharukh', 'Saif'];
   return (
     <div className="App">
+      {
+        names.map(name => <li>{name}</li>)
+      }
+      {
+        // names.map(name => <Person name={name}></Person>)
+      }
+
+      {
+        singers.map(singer => <Person name={singer.name} song={singer.song}></Person>)
+      }
+
       {/* each person is a component */}
-      <Person name="Rubel" job="circketer"></Person>
-      <Person name="Leon" job="student"></Person>
-      <Person name="Tahjib"></Person>
+      {/* <Person name={names[0]} job="circketer"></Person>
+      <Person name={names[1]} job="student"></Person>
+      <Person name={names[2]}></Person> */}
 
       <h5>New Component</h5>
-      <p>Creating new Component</p>
+      <p>Creating two Components</p>
       <Friend name="Alen" phone="018545454"></Friend>
       <Friend name="Jimmy" phone="012452085"></Friend>
 
-    </div>
+    </div >
   );
 }
 
@@ -23,7 +42,8 @@ function Person(props) {
   return (
     <div className='person'>
       <h1>{props.name}</h1>
-      <p>profession is {props.job}</p>
+      <h2>{props.song}</h2>
+      {/* <p>profession is {props.job}</p> */}
     </div>
   )
 }
