3d212e1a5f8d6780183abf27ecfd47177a1ef46d
leon0113 <tahjib.leon399@gmail.com>
2022-10-28 22:33:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* each person is a component */}
+      <Person name="Rubel" job="circketer"></Person>
+      <Person name="Leon" job="student"></Person>
+      <Person name="Tahjib"></Person>
+
+      <h5>New Component</h5>
+      <p>Creating new Component</p>
+      <Friend name="Alen" phone="018545454"></Friend>
+      <Friend name="Jimmy" phone="012452085"></Friend>
+
     </div>
   );
+}
+
+function Person(props) {
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>profession is {props.job}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  console.log(props);
+  return (
+    <div className='container'>
+      <h3>Name : {props.name}</h3>
+      <p>Phone Number : {props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
